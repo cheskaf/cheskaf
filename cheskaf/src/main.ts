231bbccf112c6649a223cc7fb294ae3999050348
Cheska Francisco <clafrncsc@gmail.com>
2024-01-18 15:24:24 +0800

    Update main.ts

--- cheskaf/src/main.ts
+++ cheskaf/src/main.ts
@@ -2,5 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './app/header/header.component';
+import { AboutComponent } from './app/about/about.component';
+import { ProjectsComponent } from './app/projects/projects.component';  
+import { ContactComponent } from './app/contact/contact.component';
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  const routes: Routes = [
+    { path: '', component: AboutComponent },
+    { path: 'about', component: AboutComponent},
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+  ];
