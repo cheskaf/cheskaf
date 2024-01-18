3bd7ecb2c418f4d3c018e0d406c2bad72b7dbf2e
Cheska Francisco <clafrncsc@gmail.com>
2024-01-18 16:06:17 +0800

    renamed app

--- portfolio/README.md
+++ portfolio/README.md
@@ -1,4 +1,4 @@
-# Cheskaf
+# portfolio
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.0.
 
--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -15,5 +15,5 @@ import { ContactComponent } from './contact/contact.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cheskaf';
+  title = 'portfolio';
 }
