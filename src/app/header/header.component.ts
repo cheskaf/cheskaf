5d4b35108e5fbec2f3b21feb4e736cf8ce1ed678
Cheska F <clafrncsc@gmail.com>
2025-01-18 22:31:59 +0800

    Updated dark mode, descriptions, footer, header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 
@@ -9,10 +9,17 @@ import { isPlatformBrowser } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   isDarkMode = false;
+  sections: HTMLElement[] = [];
+  links: HTMLElement[] = [];
+  observer: IntersectionObserver | null = null;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
@@ -22,6 +29,23 @@ export class HeaderComponent implements OnInit {
         this.updateDarkModeClass();
         this.updateLogo();
       }
+
+      this.sections = Array.from(document.querySelectorAll('section'));
+      this.links = Array.from(this.el.nativeElement.querySelectorAll('nav a'));
+    }
+  }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.observer = new IntersectionObserver(this.onIntersection.bind(this), {
+        threshold: 0.5
+      });
+
+      this.sections.forEach(section => {
+        if (this.observer) {
+          this.observer.observe(section);
+        }
+      });
     }
   }
 
@@ -34,14 +58,13 @@ export class HeaderComponent implements OnInit {
     this.updateLogo();
   }
 
-  updateDarkModeClass() {    
+  updateDarkModeClass() {
     const aboutSection = document.getElementById('about');
     const projectSection = document.getElementById('projects');
     const contactSection = document.getElementById('contact');
-    
+
     if (this.isDarkMode) {
       document.documentElement.classList.add('dark');
-      // remove light mode classes from sections
       if (aboutSection) {
         aboutSection.classList.remove('grid-bg');
         aboutSection.classList.add('grid-bg-dark');
@@ -56,7 +79,6 @@ export class HeaderComponent implements OnInit {
       }
     } else {
       document.documentElement.classList.remove('dark');
-      // add light mode classes to sections
       if (aboutSection) {
         aboutSection.classList.remove('grid-bg-dark');
         aboutSection.classList.add('grid-bg');
@@ -71,7 +93,7 @@ export class HeaderComponent implements OnInit {
       }
     }
   }
-  
+
   updateLogo() {
     const logo = document.getElementById('about-logo') as HTMLImageElement;
     if (logo) {
@@ -81,5 +103,21 @@ export class HeaderComponent implements OnInit {
         logo.src = "../../../assets/svg/logo-light-md.svg";
       }
     }
+  }
+
+  onIntersection(entries: IntersectionObserverEntry[]) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const currentSection = entry.target as HTMLElement;
+        this.links.forEach(link => {
+          this.renderer.removeClass(link, 'text-amaranth');
+          this.renderer.removeClass(link, 'font-bold');
+          if (link.getAttribute('href') === `#${currentSection.id}`) {
+            this.renderer.addClass(link, 'text-amaranth');
+            this.renderer.addClass(link, 'font-bold');
+          }
+        });
+      }
+    });
   }
 }
--- src/projects.ts
+++ src/projects.ts
@@ -4,69 +4,88 @@ export interface Project {
     id: number;
     title: string;
     date: string;
+    role: string;
     skills: Skill[];
     description: string;
-    imagepath: string;
+    thumbnailpath: string;
+    images: string[];
     featured: boolean;
+    link: string;
 }
 
 function findSkills(skillNames: string[]): Skill[] {
     return SKILLS.filter(skill => skillNames.includes(skill.title));
 }
 
-export const PROJECTS: Project[] = [
+const projectsData = [    
     {
-        id: 3,
-        title: 'BlackForest: Recruitment System and Applicant Portal',
-        date: 'March 2024 – May 2024',
+        title: 'BlackForest',
+        date: 'May 2024',
+        role: 'Frontend Developer, QA',
         description: 'A recruitment system and an applicant portal, with 4 users: HR manager, requestor, applicant, and admin.',
         skills: findSkills(['Node.js', 'SharePoint', 'Bootstrap']),
-        imagepath: '../../../assets/img/project-blackforest-1.png',
+        thumbnailpath: '../../../assets/img/project-blackforest-1.png',
+        images: [],
         featured: false,
+        link: '',
     },
     {
-        id: 2,
-        title: 'SnapCatch: Customer Training Registration',
-        date: 'Feb 2024 – March 2024',
+        title: 'SnapCatch',
+        date: 'Feb 2024',
+        role: 'Full Stack Developer',
         description: 'A customer training management system with a registration form and admin panel to manage registrations and trainings.',
         skills: findSkills(['Node.js', 'Express','SharePoint', 'Bootstrap']),
-        imagepath: '../../../assets/img/project-snapcatch-2.png',
+        thumbnailpath: '../../../assets/img/project-snapcatch-2.png',
+        images: [],
         featured: false,
+        link: '',
     },
     {
-        id: 1,
-        title: 'ArtisanAI: Stitch Counter and Crochet Community App',
-        date: 'Jul 2023 – Jan 2024',
-        description: 'A progressive web application (PWA) for crochet enthusiasts to interact with their fellow hobbyists, packed with helpful tools and resources. Integrated with an AI-enhanced stitch counter that utilizes YOLOv8 for object detection.',
-        skills: findSkills(['Python', 'Django', 'Bootstrap']),
-        imagepath: '../../../assets/img/project-artisanai-1.png',
-        featured: true,
-    },
-    {
-        id: 6,
         title: 'Portfolio V1',
-        date: 'June 2024 – July 2024',
-        description: 'A movie database with a search function, movie details, and user reviews.',
-        skills: findSkills(['Angular', 'Tailwind CSS', 'Bootstrap']),
-        imagepath: '../../../assets/img/project-portfolio-1.png',
+        date: '2024',
+        role: 'Full Stack Developer',
+        description: 'My portfolio site! Built with Angular and Tailwind CSS.',
+        skills: findSkills(['Angular', 'Tailwind CSS']),
+        thumbnailpath: '../../../assets/img/project-portfolio-1.png',
+        images: [],
         featured: false,
+        link: 'https://github.com/cheskaf/cheskaf',
     },
     {
-        id: 4,
+        title: 'ArtisanAI',
+        date: '2023 – 2024', // Jul 2023 – Jan 2024
+        role: 'Frontend Developer, UI/UX, ML',
+        description: 'This capstone project is a progressive web application (PWA) for crochet enthusiasts to interact with their fellow hobbyists. Packed with helpful tools, resources, and a stitch counter that utilizes YOLOv8 object detection.',
+        skills: findSkills(['Python', 'Django', 'Bootstrap']),
+        thumbnailpath: '../../../assets/img/project-artisanai-1.png',
+        images: [],
+        featured: true,
+        link: 'https://github.com/cheskaf/artisanai',
+    },
+    {
         title: 'Sushi Restaurant Management System',
-        date: 'May 2024 – June 2024',
+        date: '2023',
+        role: 'Frontend Developer',
         description: 'A restaurant management system with a menu, order form, and admin panel to manage orders and menu items.',
         skills: findSkills(['PHP', 'Bootstrap']),
-        imagepath: '../../../assets/img/project-sushi-1.png',
+        thumbnailpath: '../../../assets/img/project-sushi-1.png',
+        images: [],
         featured: false,
+        link: '',
     },
     {
-        id: 5,
-        title: 'College Management System: Chairperson Portal',
-        date: 'June 2024 – July 2024',
+        title: 'College Management System',
+        date: '2022',
+        role: 'Frontend Developer',
         description: 'A college management system with a chairperson portal to manage students, faculty, and courses.',
         skills: findSkills(['Python', 'Django', 'Bootstrap']),
-        imagepath: '../../../assets/img/project-college-1.png',
+        thumbnailpath: '../../../assets/img/project-college-1.png',
+        images: [],
         featured: false,
+        link: '',
     },
 ];
+export const PROJECTS: Project[] = projectsData.map((project, index) => ({
+    id: index + 1,
+    ...project,
+}));   
